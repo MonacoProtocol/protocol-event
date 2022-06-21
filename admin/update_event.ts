512b9d3ab5c1588cf7b3e68fe983b4b3a2ad8e00
Eoin Murphy <95632764+eoin-betdex@users.noreply.github.com>
2022-06-21 13:59:16 +0100

    Add basic admin client functionality (#33)
    * Add create event script
    
    * Add updateEvent admin methods

---
+++ admin/update_event.ts
@@ -0,0 +1,207 @@
+import * as anchor from "@project-serum/anchor";
+import { findEventPda, getProgram, getAnchorProvider } from "./util";
+
+if (process.argv.length < 4) {
+  console.log(
+    "Usage: yarn run updateEvent <SLUG> <FIELD|COMMAND> [<PARAM>, ...]",
+  );
+  process.exit(1);
+}
+
+const slug = process.argv[2];
+const updateTask = process.argv[3];
+
+switch (updateTask) {
+  case "score":
+    updateScore();
+    break;
+  case "startTime":
+    updateStartTime();
+    break;
+  case "period":
+    updatePeriod();
+    break;
+  case "participants":
+    updateParticipants();
+    break;
+  case "activate":
+    activate();
+    break;
+  case "deactivate":
+    deactivate();
+    break;
+  case "start":
+    start();
+    break;
+  case "complete":
+    complete();
+    break;
+  default:
+    console.error(
+      "Available fields to update : score, startTime, period, participants\nAvailable update commands: activate, deactivate, start, complete",
+    );
+}
+
+function updateScore() {
+  if (process.argv.length < 5) {
+    console.log("Expected score string missing");
+    process.exit(1);
+  }
+  const score = process.argv[4];
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .updateScore(slug, score)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function updateStartTime() {
+  if (process.argv.length < 5) {
+    console.log("Expected startTimestamp in seconds missing");
+    process.exit(1);
+  }
+  const startTime = new anchor.BN(Number.parseInt(process.argv[4]));
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .setStartTimestamp(slug, startTime)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function updatePeriod() {
+  if (process.argv.length < 5) {
+    console.log("Expected period missing");
+    process.exit(1);
+  }
+  const period = Number.parseInt(process.argv[4]);
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .updatePeriod(slug, period)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function updateParticipants() {
+  if (process.argv.length < 5) {
+    console.log("Expected participants string missing");
+    process.exit(1);
+  }
+  const participants = JSON.parse(process.argv[4]);
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .updateParticipants(slug, participants)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function activate() {
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .activateEvent(slug)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function deactivate() {
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .deactivateEvent(slug)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function start() {
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .startEvent(slug)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
+
+function complete() {
+  getProgram().then(async (program) => {
+    const provider = getAnchorProvider();
+    const eventPk = await findEventPda(slug, program);
+    await program.methods
+      .completeEvent(slug)
+      .accounts({
+        event: eventPk,
+        authority: provider.wallet.publicKey,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  });
+}
