0723885d4560ca080c3c8e88e11a0cb8e3ddd1db
obi-ewan <ewan@betdex.com>
2023-06-14 11:00:30 +0100

    refactor: event account refactor v1

---
+++ programs/protocol_event/src/state/event.rs
@@ -0,0 +1,34 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Event {
+    pub authority: Pubkey,
+    pub payer: Pubkey,
+
+    pub name: String, // for display purposes e.g. Los Angeles Football Club vs. LA Galaxy
+    pub active: bool,
+
+    pub category: Category,
+    pub event_group: EventGroup,
+
+    pub participants: Vec<Participant>,
+
+    pub expected_start_timestamp: i64,
+    pub expected_end_timestamp: i64,
+}
+
+pub struct Category {
+    pub id: String, // category identifier e.g. FOOTBALL
+    pub name: String, // for display purposes e.g Football
+}
+
+pub struct EventGroup {
+    pub id: String, // event group identifier e.g. MLS
+    pub name: String, // for display purposes e.g. Major League Soccer
+}
+
+pub struct Participant {
+    pub id: String, // participant identifier e.g. LAFC
+    pub name: String, // for display purposes e.g. Los Angeles Football Club
+    pub participant_members: Vec<String>, // list of participant members e.g. LAFC players; [Carlos Vela, Diego Rossi]
+}
