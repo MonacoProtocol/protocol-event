c66d95f76471446b4d2a3e382a88ad7023a2c557
Ewan <ewan@betdex.com>
2023-07-19 17:09:06 +0100

    feat: files for event client publishing (#23)
    * feat: files for event client publishing
    
    * refactor: rename client
    
    * chore: export from a main index.ts
    
    * refactor: use updated package name

--- client/queries/index.ts
+++ client/queries/index.ts
@@ -1,1 +1,4 @@
 export { Events } from "./eventQuery";
+export { Categories } from "./categoryQuery";
+export { EventGroups } from "./eventGroupQuery";
+export { Participants } from "./participantQuery";
